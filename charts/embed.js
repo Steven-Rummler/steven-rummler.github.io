5807b0e5219cc5ea3c0b41ea666f56abc432dab8
Steven Rummler <steven.rummler@gmail.com>
2023-10-01 12:53:09 -0600

    reorganize charts into tiles

--- charts/embed.js
+++ charts/embed.js
@@ -1,140 +1,186 @@
-const urlParams = new URLSearchParams(window.location.search);
-const engine = urlParams.get('engine');
-const url = engine === 'canvasjs' ? 'https://cdn.canvasjs.com/canvasjs.min.js' : 'https://cdn.jsdelivr.net/npm/apexcharts';
-const callback = engine === 'canvasjs' ? renderCanvasJSChart : renderApexChart;
+// Extract params from URL
+const params = new URLSearchParams(window.location.search);
+const chart = params.get('chart');
+const [engine, type] = chart.split('-');
+const format = params.get('format');
+const formatter = format === 'currency' ? Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format :
+  format === 'percent' ? Intl.NumberFormat('en-US', { style: 'percent', maximumFractionDigits: 1, minimumFractionDigits: 0 }).format :
+    Intl.NumberFormat('en-US').format;
+const data = params.get('data').split(',').map((point) => ({ x: point.split(':')[0], y: parseFloat(point.split(':')[1]) }));
+const chartJSData = data.map((point) => ({ label: point.x, y: point.y, indexLabel: formatter(point.y) }));
 
+// Set up chart options
+const apexChartsBarOptions = {
+  chart: {
+    type: 'bar',
+    height: '100%',
+    width: '100%',
+    toolbar: {
+      show: false
+    }
+  },
+  plotOptions: {
+    bar: {
+      horizontal: true,
+      dataLabels: {
+        position: 'top'
+      },
+      barHeight: '60%'
+    }
+  },
+  series: [{
+    name: 'Series',
+    data
+  }],
+  colors: ['#5DA6B0'],
+  xaxis: {
+    labels: {
+      show: format === 'percent',
+      formatter
+    },
+    max: format === 'percent' ? 1 : data.reduce((max, next) => Math.max(max, next.y), 0) * 1.1,
+    min: 0,
+    tickAmount: format === 'percent' ? 5 : undefined,
+    axisTicks: {
+      show: false
+    }
+  },
+  grid: {
+    xaxis: { lines: { show: format === 'percent' } },
+    yaxis: { lines: { show: false } }
+  },
+  tooltip: {
+    enabled: false
+  },
+  dataLabels: {
+    formatter,
+    style: {
+      colors: ['#222'],
+      fontWeight: null
+    },
+    textAnchor: 'end',
+    offsetX: 10
+  },
+  fill: {
+    opacity: 1
+  }
+};
+const apexChartsLineOptions = {
+  chart: {
+    type: 'line',
+    height: '100%',
+    width: '100%',
+    toolbar: {
+      show: false
+    }
+  },
+  series: [{
+    name: 'Series',
+    data
+  }],
+  colors: ['#5DA6B0'],
+  yaxis: {
+    labels: {
+      show: format === 'percent',
+      formatter
+    },
+    max: format === 'percent' ? 1 : data.reduce((max, next) => Math.max(max, next.y), 0) * 1.1,
+    min: 0,
+    tickAmount: format === 'percent' ? 5 : undefined,
+    axisTicks: {
+      show: false
+    }
+  },
+  grid: {
+    xaxis: { lines: { show: format === 'percent' } },
+    yaxis: { lines: { show: false } }
+  },
+  tooltip: {
+    enabled: false
+  },
+  dataLabels: {
+    formatter,
+    style: {
+      colors: ['#222'],
+      fontWeight: null
+    },
+    textAnchor: 'end',
+    offsetX: 10
+  },
+  fill: {
+    opacity: 1
+  }
+};
+const canvasJSBarOptions = {
+  animationEnabled: true,
+  data: [{
+    type,
+    color: "#5DA6B0",
+    dataPoints: chartJSData,
+    indexLabelPlacement: 'inside',
+    indexLabelTextAlign: 'right'
+  }],
+  axisX: {
+    tickLength: 0,
+    labelMaxWidth: 200,
+    labelAngle: 0,
+    lineColor: '#ddd',
+  },
+  axisY: {
+    minimum: 0,
+    maximum: format === 'percent' ? 1 : data.reduce((max, next) => Math.max(max, next.y), 0) * 1.1,
+    gridThickness: format === 'percent' ? 1 : 0,
+    gridColor: '#ddd',
+    labelFormatter: format === 'percent' ? label => formatter(label.value) : () => '',
+    lineThickness: 0,
+    tickLength: 0
+  },
+  dataPointWidth: 20,
+  toolTip: { enabled: false }
+};
+const canvasJSLineOptions = {
+  animationEnabled: true,
+  data: [{
+    type,
+    color: "#5DA6B0",
+    dataPoints: chartJSData,
+    indexLabelPlacement: 'inside',
+    indexLabelTextAlign: 'right'
+  }],
+  axisX: {
+    tickLength: 0,
+    labelMaxWidth: 200,
+    labelAngle: -90,
+    lineColor: '#ddd',
+  },
+  axisY: {
+    minimum: 0,
+    maximum: format === 'percent' ? 1 : data.reduce((max, next) => Math.max(max, next.y), 0) * 1.1,
+    gridThickness: format === 'percent' ? 1 : 0,
+    gridColor: '#ddd',
+    labelFormatter: format === 'percent' ? label => formatter(label.value) : () => '',
+    lineThickness: 0,
+    tickLength: 0
+  },
+  dataPointWidth: 20,
+  toolTip: { enabled: false }
+};
+
+// Select engine and renderer
+const url = engine === 'canvasjs' ? 'https://cdn.canvasjs.com/canvasjs.min.js' : 'https://cdn.jsdelivr.net/npm/apexcharts';
+const options = {
+  'apexcharts-bar': apexChartsBarOptions,
+  'apexcharts-line': apexChartsLineOptions,
+  'canvasjs-bar': canvasJSBarOptions,
+  'canvasjs-line': canvasJSLineOptions,
+}[chart];
+
+// Append script
 const script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = url;
-script.onload = callback;
+script.onload = () => {
+  if (engine === 'apexcharts') new ApexCharts(document.querySelector('#chartContainer'), options).render();
+  if (engine === 'canvasjs') new CanvasJS.Chart("chartContainer", options).render();
+}
 document.body.appendChild(script);
-
-function renderCanvasJSChart() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const format = urlParams.get('format');
-  const formatter = format === 'currency' ? Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format :
-    format === 'percent' ? Intl.NumberFormat('en-US', { style: 'percent', maximumFractionDigits: 1, minimumFractionDigits: 0 }).format :
-      Intl.NumberFormat('en-US').format;
-
-  const urlData = urlParams.get('data');
-  if (urlData === null) return;
-  const dataPoints = urlData.split(',').map((dataPoint) => {
-    const [label, y] = dataPoint.split(':');
-    return { label, y: parseFloat(y), indexLabel: formatter(parseFloat(y)) };
-  }).sort((a, b) => b.y - a.y);
-
-  const chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-
-    data: [{
-      type: "bar",
-      color: "#5DA6B0",
-      dataPoints,
-      indexLabelPlacement: 'inside',
-      indexLabelTextAlign: 'right'
-    }],
-
-    axisX: {
-      tickLength: 0,
-      labelMaxWidth: 200,
-      lineColor: '#ddd',
-    },
-
-    axisY: {
-      minimum: 0,
-      maximum: format === 'percent' ? 1 : dataPoints[0].y * 1.1,
-      gridThickness: format === 'percent' ? 1 : 0,
-      gridColor: '#ddd',
-      labelFormatter: format === 'percent' ? label => formatter(label.value) : () => '',
-      lineThickness: 0,
-      tickLength: 0
-    },
-
-    dataPointWidth: 20,
-
-    toolTip: {
-      enabled: false
-    }
-  });
-  chart.render();
-}
-
-function renderApexChart() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const format = urlParams.get('format');
-  const formatter = format === 'currency' ? Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 0 }).format :
-    format === 'percent' ? Intl.NumberFormat('en-US', { style: 'percent', maximumFractionDigits: 1, minimumFractionDigits: 0 }).format :
-      Intl.NumberFormat('en-US').format;
-
-  const urlData = urlParams.get('data');
-  if (urlData === null) return;
-  const dataPoints = urlData.split(',').map((dataPoint) => {
-    const [label, y] = dataPoint.split(':');
-    return { label, y: parseFloat(y), indexLabel: formatter(parseFloat(y)) };
-  });
-
-  const chart = new ApexCharts(document.querySelector('#chartContainer'), {
-    chart: {
-      type: 'bar',
-      height: '100%',
-      width: '100%',
-      toolbar: {
-        show: false
-      }
-    },
-    plotOptions: {
-      bar: {
-        horizontal: true,
-        dataLabels: {
-          position: 'top'
-        },
-        barHeight: '60%'
-      }
-    },
-    series: [{
-      name: 'Series',
-      data: dataPoints.map(({ label, y }) => ({ x: label, y }))
-    }],
-    colors: ['#5DA6B0'],
-    xaxis: {
-      labels: {
-        show: format === 'percent',
-        formatter
-      },
-      max: format === 'percent' ? 1 : dataPoints.reduce((max, next) => Math.max(max, next.y), 0) * 1.1,
-      min: 0,
-      tickAmount: format === 'percent' ? 5 : undefined,
-      axisTicks: {
-        show: false
-      }
-    },
-    grid: {
-      yaxis: {
-        lines: {
-          show: false
-        }
-      },
-      xaxis: {
-        lines: {
-          show: format === 'percent'
-        }
-      }
-    },
-    tooltip: {
-      enabled: false
-    },
-    dataLabels: {
-      formatter,
-      style: {
-        colors: ['#222'],
-        fontWeight: null
-      },
-      textAnchor: 'end',
-      offsetX: 10
-    },
-    fill: {
-      opacity: 1
-    }
-  });
-  chart.render();
-}
--- charts/script.js
+++ charts/script.js
@@ -1,42 +1,40 @@
 // const embed = 'http://localhost:5500/charts/embed.html';
 const embed = 'https://stevenrummler.com/charts/embed';
-let engine = "apexcharts";
+
+const charts = ['apexcharts-bar', 'apexcharts-line', 'canvasjs-bar', 'canvasjs-line'];
+
 let data = "Undergraduate certificate:17976,Associates degree:25608,Bachelor's degree:32568,Master's degree:48468,Doctoral degree:53112";
 let format = "currency";
 let height = 200;
 let width = 400;
 
-document.querySelector("#engine").addEventListener("change", () => {
-  engine = document.querySelector("#engine").value;
-  update();
-});
-
 document.querySelector("#dataFormat").addEventListener("change", () => {
   const value = document.querySelector("#dataFormat").value;
   data = value.split("&format=")[0];
   format = value.split("&format=")[1];
-  update();
+  charts.forEach(update);
 });
 
 document.querySelector("#height").addEventListener("change", () => {
   height = document.querySelector("#height").value;
-  update();
+  charts.forEach(update);
 });
 
 document.querySelector("#width").addEventListener("change", () => {
   width = document.querySelector("#width").value;
-  update();
+  charts.forEach(update);
 });
 
-function update() {
-  const newURL = `${embed}?engine=${engine}&data=${data}&format=${format}`;
-  document.querySelector("#dynamic").src = newURL;
-  document.querySelector("#dynamic").height = `${height}px`;
-  document.querySelector("#dynamic").width = `${width}px`;
-  document.querySelector('code').innerText = `<iframe src="${newURL}" height="${height}px" width="${width}px" style="border: none"></iframe>`;
+function update(id) {
+  const newURL = `${embed}?chart=${id}&data=${data}&format=${format}`;
+  const chart = document.querySelector(`#${id}`);
+  chart.src = newURL;
+  chart.height = `${height}px`;
+  chart.width = `${width}px`;
+  chart.parentElement.parentElement.children[1].innerText = `<iframe src="${newURL}" height="${height}px" width="${width}px" style="border: none"></iframe>`;
 }
 
-update();
+charts.forEach(update);
 
 document.querySelector('.copy').addEventListener('click', () => {
   const code = document.querySelector('code').innerText;
